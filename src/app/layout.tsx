d2412ad87b65c3a8978f818868d8b5c32dba4b4e
Kevin Simon <keviniansimon@gmail.com>
2023-06-07 09:47:16 +0200

    fix: fixed footer import name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/footer/footer";
+import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import { Providers } from "../lib/providers/ThemeProvider/providers";
 import {
