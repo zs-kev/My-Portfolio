fd33d8b4f3c3b7437e79a63686f6f2568fcf1ba6
Kevin Simon <keviniansimon@gmail.com>
2023-06-28 13:33:51 +0200

    build: added mobile menu functionality

--- src/components/header/navigation/Navigation.tsx
+++ src/components/header/navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./Navigation.module.css";
@@ -8,29 +9,69 @@ const pagesArr: { page: string; path: string }[] = [
   { page: "Home", path: "/" },
   { page: "About", path: "/about" },
   { page: "Portfolio", path: "/portfolio" },
-  { page: "Articles", path: "/portfolio" },
+  { page: "Articles", path: "/articles" },
   { page: "Contact", path: "/contact" },
 ];
 
-export interface NavigationProps {}
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
 
-const Navigation: React.FC<NavigationProps> = () => {
+const variantsItems = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+export interface NavigationProps {
+  handleNavClick?: () => void;
+  isNavOpen?: Boolean;
+}
+
+const Navigation: React.FC<NavigationProps> = ({
+  handleNavClick,
+  isNavOpen = true,
+}) => {
   const currentRoute = usePathname();
 
   return (
-    <nav className={styles.nav}>
+    <motion.nav
+      className={styles.nav}
+      variants={variants}
+      initial={"closed"}
+      animate={isNavOpen ? "open" : "closed"}
+      exit={"closed"}
+    >
       {pagesArr.map(({ page, path }) => (
-        <Link
-          key={page}
-          href={path}
-          className={`${styles.navLink} ${
-            currentRoute === path && styles.active
-          }`}
-        >
-          {page}
-        </Link>
+        <motion.div key={page} variants={variantsItems}>
+          <Link
+            onClick={handleNavClick}
+            href={path}
+            className={`${styles.navLink} ${
+              currentRoute === path && styles.active
+            }`}
+          >
+            {page}
+          </Link>
+        </motion.div>
       ))}
-    </nav>
+    </motion.nav>
   );
 };
 
