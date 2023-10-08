b6f19c19b3132002f6693d9a475577342ba137e8
Kevin Simon <keviniansimon@gmail.com>
2023-10-08 21:53:14 +0200

    ci: setup sanity schema

---
+++ src/sanity/schemas/client.ts
@@ -0,0 +1,54 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "client",
+  title: "Client",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "mainLogo",
+      title: "Main Logo",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "altLogo",
+      title: "Alt Logo",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    {
+      name: "clientColor",
+      title: "Client color",
+      type: "color",
+    },
+  ],
+});
--- src/sanity/schemas/post.ts
+++ src/sanity/schemas/post.ts
@@ -1,72 +1,217 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
+  groups: [
+    {
+      name: "hero",
+      title: "Hero",
+    },
+    {
+      name: "overview",
+      title: "Overview",
+    },
+    {
+      name: "features",
+      title: "Features",
+    },
+    {
+      name: "quote",
+      title: "Quote",
+    },
+    {
+      name: "images",
+      title: "Images",
+    },
+    {
+      name: "final",
+      title: "Final Words",
+    },
+  ],
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
+      group: "hero",
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      group: "hero",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      title: "Author",
+      type: "reference",
+      group: "hero",
+      to: { type: "author" },
     }),
     defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
+      name: "client",
+      title: "Client",
+      type: "reference",
+      group: "hero",
+      to: { type: "client" },
+    }),
+    defineField({
+      name: "CompletedAt",
+      title: "Completed",
+      type: "datetime",
+      group: "hero",
+    }),
+    defineField({
+      name: "websiteUrl",
+      title: "Website URL",
+      type: "url",
+      group: "hero",
+    }),
+    defineField({
+      name: "githubUrl",
+      title: "Github URL",
+      type: "url",
+      group: "hero",
+    }),
+    defineField({
+      name: "theChallenge",
+      title: "The Challenge",
+      type: "text",
+      group: "overview",
+    }),
+    defineField({
+      name: "myApproach",
+      title: "My Approach",
+      type: "text",
+      group: "overview",
+    }),
+    defineField({
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      group: "overview",
       options: {
         hotspot: true,
       },
       fields: [
         {
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative Text',
-        }
-      ]
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
     }),
     defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      name: "quote",
+      title: "Quote",
+      type: "text",
+      group: "quote",
     }),
     defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
+      name: "quoteName",
+      title: "Quote Name",
+      type: "text",
+      group: "quote",
     }),
     defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: "firstImage",
+      title: "First image",
+      type: "image",
+      group: "images",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "secondImage",
+      title: "Second image",
+      type: "image",
+      group: "images",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "thirdImage",
+      title: "Third image",
+      type: "image",
+      group: "images",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "fourthImage",
+      title: "Fourth image",
+      type: "image",
+      group: "images",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "finalWords",
+      title: "Final Words",
+      type: "text",
+      group: "final",
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      group: "hero",
+      of: [{ type: "reference", to: { type: "category" } }],
+    }),
+    defineField({
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
     }),
   ],
 
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
     },
   },
-})
+});
