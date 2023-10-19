51ed238e02bcffe857e2c068843b1ad794431ce4
Kevin Simon <keviniansimon@gmail.com>
2023-10-19 17:58:39 +0200

    style: changed to responsive font sizes

--- src/components/portfolio/allItems/PortfolioList.tsx
+++ src/components/portfolio/allItems/PortfolioList.tsx
@@ -1,6 +1,7 @@
 import urlFor from "@/lib/urlFor";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./PortfolioList.module.css";
 
 export interface PortfolioListProps {
   portfolioItems: any;
@@ -8,23 +9,23 @@ export interface PortfolioListProps {
 
 const PortfolioList: React.FC<PortfolioListProps> = ({ portfolioItems }) => {
   return (
-    <div>
+    <div className={styles.portfolioList}>
       {portfolioItems.map((item: any) => (
         <Link href={item.slug.current} key={item.title}>
           <div>
             <Image
               src={urlFor(item.featureImage.asset).url()}
               alt=""
-              width={271}
-              height={99}
+              width={330}
+              height={400}
             />
           </div>
           <h3>{item.client.title}</h3>
-          {item.categories.map((category: any, index: number) => (
-            <div key={index}>
-              <p>{category.title}</p>
-            </div>
-          ))}
+          <div className={styles.categoryWrapper}>
+            {item.categories.map((category: any, index: number) => (
+              <p key={index}>{category.title}</p>
+            ))}
+          </div>
         </Link>
       ))}
     </div>
